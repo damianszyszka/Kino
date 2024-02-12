536a80d8dfc92dd9ad241b9a24feb41b28dac93f
LewandowskinCDV <nlewandowski6721@gmail.com>
2024-02-12 20:41:20 +0100

    DB

--- KinoSystemRezerwacji/Program.cs
+++ KinoSystemRezerwacji/Program.cs
@@ -18,6 +18,7 @@ builder.Services.AddControllersWithViews();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Configuration.AddEnvironmentVariables();
 builder.Services.AddDbContext<CinemaContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("CinemaDatabase")));
 
